51cbafaef3c75586d162cbd9c9a4c6c529eebd71
Leonardo Ferreira de Almeida <65037071+LeonardoFerreira1209@users.noreply.github.com>
2022-06-20 13:56:28 -0300

    Delete INFRAESTRUTURE directory

--- INFRAESTRUTURE/FACADES/CEP/ICepFacade.cs
+++
@@ -1,9 +0,0 @@
-﻿using DOMAIN.REQUEST;
-using DOMAIN.RESPONSE;
-
-namespace INFRAESTRUTURE.FACADES.CEP;
-
-public interface ICepFacade
-{
-    Task<(bool sucesso, CepResponse response)> GetCep(CepRequest request);
-}
