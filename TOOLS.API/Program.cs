60ab732e338bc5413c578185ddc0473c7876ed48
Leonardo Almeida <Leo.Ferreira30@outlook.com>
2022-06-30 19:57:17 -0300

    cORS ADICIONADO

--- TOOLS.API/Program.cs
+++ TOOLS.API/Program.cs
@@ -57,6 +57,7 @@ try
         .UseStaticFiles()
         .UseDefaultFiles()
         .UseRouting()
+        .UseCors("CorsPolicy")
         .UseAuthorization()
         .ConfigureHealthChecks()
         .UseSwaggerConfigurations(configurations)
